5a6e7d2360dda2bd165535f6bc8f0a6c46f5a1ac
dxmooremelb <danielmoore.melb@gmail.com>
2024-03-06 06:15:58 +1100

    added conversion for celsisus to fahrenheit, vice versa

--- index.js
+++ index.js
@@ -1,3 +1,104 @@
+import dayjs from "dayjs";
+
+function handleDOM(location) {
+  const selectedLocation = location.resolvedAddress;
+
+  location.days.forEach((day, index) => {
+    const lowTemp = `L: ${day.tempmin}`;
+    const maxTemp = `H: ${day.tempmax}`;
+    const currentTemp = day.temp;
+    const conditions = day.conditions;
+    const description = day.description;
+    const icon = day.icon;
+    const date = dayjs(day.date).format("ddd");
+
+    if (index === 0) {
+      createDOMElement("today-weather", "enteredLocation", selectedLocation);
+      createDOMElement("today-weather", "currentTemp", currentTemp, "temp");
+      createDOMElement("today-weather", "currentConditions", conditions);
+      createDOMElement("today-weather", "todayLowTemp", lowTemp, "temp");
+      createDOMElement("today-weather", "todayMaxTemp", maxTemp, "temp");
+      createDOMElement("today-weather", "currentDescription", description);
+    } else {
+      createDOMElement(`day-${index}`, `day${index}Date`, date);
+      createDOMElement(`day-${index}`, `day${index}Icon`, "", null, icon);
+      createDOMElement(`day-${index}`, `day${index}Low`, lowTemp, "temp");
+      createDOMElement(`day-${index}`, `day${index}Max`, maxTemp, "temp");
+    }
+  });
+
+  switchTempListener();
+}
+
+function switchTempListener(element, text) {
+  const switchBtn = document.querySelector(".switcher");
+  switchBtn.classList.add("celsius");
+  const temps = document.querySelectorAll(".temp");
+
+  switchBtn.addEventListener("click", () => {
+    temps.forEach((temp, index) => {
+      if (index === 0) {
+        let num = temp.innerText;
+        num = num.substring(0, num.length - 1);
+
+        if (switchBtn.classList.contains("celsius")) {
+          const fahrenheit = Math.round(((num * 9) / 5 + 32) * 10) / 10;
+          temp.innerText = fahrenheit + "°";
+        } else if (switchBtn.classList.contains("fahrenheit")) {
+          const celsius = Math.round((((num - 32) * 5) / 9) * 10) / 10;
+          temp.innerText = celsius + "°";
+        }
+      }
+
+      if (index !== 0) {
+        const array = temp.innerText.split(":");
+        array[1] = array[1].substring(0, array[1].length - 1);
+
+        if (switchBtn.classList.contains("celsius")) {
+          const fahrenheit = Math.round(((array[1] * 9) / 5 + 32) * 10) / 10;
+          temp.innerText = array[0] + ": " + fahrenheit + "°";
+          if (index === temps.length - 1) {
+            switchBtn.classList.remove("celsius");
+            switchBtn.classList.add("fahrenheit");
+            switchBtn.innerText = "Celsius";
+          }
+        } else if (switchBtn.classList.contains("fahrenheit")) {
+          const celsius = Math.round((((array[1] - 32) * 5) / 9) * 10) / 10;
+          temp.innerText = array[0] + ": " + celsius + "°";
+          if (index === temps.length - 1) {
+            switchBtn.classList.remove("fahrenheit");
+            switchBtn.classList.add("celsius");
+            switchBtn.innerText = "Fahrenheit";
+          }
+        }
+      }
+    });
+  });
+
+  // const fahrenheit = (temp[1] * 9) / 5 + 32;
+
+  // const celsius = ((temp[1] - 32) * 5) / 9;
+}
+
+function createDOMElement(parent, className, text, temp, icon) {
+  const target = document.querySelector(`.${parent}`);
+
+  if (document.querySelector(`.${className}`)) {
+    const removeEl = document.querySelector(`.${className}`);
+    target.removeChild(removeEl);
+  }
+
+  const element = document.createElement("div");
+
+  element.classList.add(`${className}`);
+  element.innerText = `${text}`;
+  if (temp) {
+    element.innerText = `${text}°`;
+    element.classList.add("temp");
+  }
+  target.appendChild(element);
+}
+
 async function parseWeatherData(response) {
   try {
     const data = await response;
@@ -25,8 +126,9 @@ async function parseWeatherData(response) {
       }
     });
 
-    console.log(location);
+    handleDOM(location);
   } catch (err) {
+    console.log(err);
     alert("Please enter a valid location");
   }
 }
@@ -36,10 +138,13 @@ async function getWeather(location) {
     let response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=KZB76SZB8M6YGEES94J6VW5R9&contentType=json`
     );
+    console.log(response);
     return parseWeatherData(response);
   } catch (err) {
     console.log(err);
   }
 }
 
-getWeather("Chabickistan");
+getWeather("Sydney");
+
+// °
